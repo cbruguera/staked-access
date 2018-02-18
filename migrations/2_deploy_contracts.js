023179cbc38e150d773e31981e5875bfaa3ee0e7
Carlos Bruguera <cbruguera@gmail.com>
2018-02-18 18:36:07 +0700

    Updated migration file with real token address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ module.exports = deployer => {
 
   const expiry = now + 2592000 // 30 days after start
   const price = 10000000000000000000 // 10 KEY
-  const tokenAddress = '0x29dc722b24a08ba7ed602219574a8b041fde92fc' // ropsten test
+  const tokenAddress = '0x4cc19356f2d37338b9802aa8e8fc58b0373296e7' // Mainnet SelfKey Token
 
   deployer.deploy(StakedAccess, expiry, price, tokenAddress)
 }
